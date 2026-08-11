6fffd98f51ad0811c130c2846c1e1b049130b5dc
Seif_Bedair <seifbedair2020@gmail.com>
2026-08-11 10:02:56 +0300

    Initial commit with Terraform and k8s manifests

---
+++ terraform/main.tf
@@ -0,0 +1,41 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-${var.prefix}-cluster"
+  location = var.location
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = replace("${var.prefix}registry${random_integer.ri.result}", "-", "")
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = false
+}
+
+resource "random_integer" "ri" {
+  min = 1000
+  max = 9999
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "${var.prefix}-cluster"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "${var.prefix}-dns"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_B2s_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+resource "azurerm_role_assignment" "aks_acr_pull" {
+  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  role_definition_name             = "AcrPull"
+  scope                            = azurerm_container_registry.acr.id
+  skip_service_principal_aad_check = true
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "prefix" {
+  type    = string
+  default = "gitops-aks"
+}
+
+variable "location" {
+  type    = string
+  default = "polandcentral"
+}
